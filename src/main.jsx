b2223dd532eb96b6f6569303d23ad0ca6be215f8
ste1v0 <109760126+ste1v0@users.noreply.github.com>
2024-01-15 14:25:37 +0300

    feat: css backdrop, dropdown

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import GamePage from './components/GamePage.jsx'
-import Layout from './components/Layout.jsx'
-import App from './components/App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <style>
-      @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;200;400;700&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;200;400;700;800;900&display=swap');
       </style>
-  <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<App />} />
-          <Route path="/games/:slug" element={<GamePage />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
   </React.StrictMode>,
 )
